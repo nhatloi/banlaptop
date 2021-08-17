b6711d4fb61d9418e1b14c885b105a0c1f0d5ed5
nhatloi <1724801030179@student.tdmu.edu.vn>
2021-08-18 02:39:52 +0700

    update

--- admin/db/connect.php
+++ admin/db/connect.php
@@ -1,6 +1,14 @@
 <?php
 // Kết nối databases
-$conn = mysqli_connect('localhost', 'root', '', 'worldlap');
+
+$Server='MYSQL5045.site4now.net';
+$Database='db_a78b0e_worldl';
+$Uid='a78b0e_worldl';
+$Password='1q2w3e4r';
+
+
+
+$conn = mysqli_connect($Server, $Uid, $Password, $Database);
 mysqli_set_charset($conn, 'UTF8');
 if (!$conn) {
     echo "Kết nối thất bại." . mysqli_connect_error();
